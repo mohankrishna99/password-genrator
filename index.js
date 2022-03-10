dca9c3d6e8235b69c68ffcaeb3ce41f9fadd797e
mohankrishna99 <kothamohankrishna9@gmail.com>
2022-03-10 12:47:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let possibleChars = [
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',1,2,3,4,5,6,7,8,9,0,'!','@','#','$','*','%','&'
+    ]
+let pwd = document.getElementById('pwd')
+function pwdGen() {
+    pwd.textContent = ''
+    for( let i = 0; i < 8; i++) {
+        pwd.textContent += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+    }
+}
