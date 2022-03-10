d14f8e60890b82988a9b3d72e14119669b646121
mohankrishna99 <kothamohankrishna9@gmail.com>
2022-03-10 13:30:38 +0530

    update
    added image for unpopulated state,

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 let possibleChars = [
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',1,2,3,4,5,6,7,8,9,0,'!','@','#','$','*','%','&'
     ]
-let pwd = document.getElementById('pwd')
+let pwd1 = document.getElementById('pwd1')
+let pwd2 = document.getElementById('pwd2')
+let pwd3 = document.getElementById('pwd3')
+let pwd4 = document.getElementById('pwd4')
 function pwdGen() {
-    pwd.textContent = ''
+    pwd1.textContent = ''
     for( let i = 0; i < 8; i++) {
-        pwd.textContent += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+        pwd1.textContent += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+    }
+
+    pwd2.textContent = ''
+    for( let i = 0; i < 8; i++) {
+        pwd2.textContent += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+    }
+
+    pwd3.textContent = ''
+    for( let i = 0; i < 8; i++) {
+        pwd3.textContent += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+    }
+
+    pwd4.textContent = ''
+    for( let i = 0; i < 8; i++) {
+        pwd4.textContent += possibleChars[Math.floor(Math.random() * possibleChars.length)]
     }
 }
